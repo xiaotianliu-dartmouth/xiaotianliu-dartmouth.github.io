f99262816743e2d151958e5332b99f94748cbe6f
xiaotianliu-dartmouth <xiaotian.liu-2.gr@dartmouth.edu>
2026-05-09 19:45:27 +0000

    Deploying to gh-pages from @ xiaotianliu-dartmouth/xiaotianliu-dartmouth.github.io@4276606891c1cf6acfbbb3433803e70701ce6dcd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Teaching assistant positions at Dartmouth College and Wake Forest University.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
